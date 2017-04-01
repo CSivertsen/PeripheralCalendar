4cf889750524bb35c2059ab771eebd025bda6c36
CSivertsen <chr@csiv.dk>
2017-04-01 16:54:41 +0200

    Added missing comma

--- tester.py
+++ tester.py
@@ -20,7 +20,7 @@ DC = 23
 SPI_PORT = 0
 SPI_DEVICE = 0
 
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
 
 #LED strip config
 LED_COUNT       = 1
